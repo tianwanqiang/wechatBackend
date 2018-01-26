7db64be8610114d77e84ba25216e640b1e1c77bb
tianwanqiang <524339509@qq.com>
2018-01-26 13:01:57 +0800

    修复启动报错，调整工程结构

--- cs-front/src/main/java/com/twq/CsFrontApplication.java
+++ cs-front/src/main/java/com/twq/CsFrontApplication.java
@@ -1,15 +1,18 @@
-package com.twq.main;//package com.twq;//package com.twq.main;
+package com.twq;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.twq.dao.mapper")
-@ComponentScan("com.twq")
 public class CsFrontApplication {
     public static void main(String[] args) {
         SpringApplication.run(CsFrontApplication.class, args);
     }
+
+
 }
+
+
+
