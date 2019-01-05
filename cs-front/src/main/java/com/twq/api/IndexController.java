1f05d711503d57ee2921f2364370c7c13997afd9
tianwanqiang <524339509@qq.com>
2019-01-05 20:41:34 +0800

    查询用户参与状态

--- cs-front/src/main/java/com/twq/api/IndexController.java
+++ cs-front/src/main/java/com/twq/api/IndexController.java
@@ -6,6 +6,7 @@ import com.twq.dao.model.ProjectInfo;
 import com.twq.dao.model.Tags;
 import com.twq.service.base.*;
 import com.twq.util.Nodes;
+import com.twq.util.USER_JOIN_STATUS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Field;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -40,8 +42,15 @@ public class IndexController {
     TagsService tagsService;
 
 
+    @Value("${begin_time}")
+    private String begin_time;
+
+    @Value("${end_time}")
+    private String end_time;
+
     @Value("${join_amount}")
     private Integer joinAmount;
+
 
     @RequestMapping(path = "/index", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map callingApi(@RequestParam Integer project_id, @RequestParam(value = "user_id", defaultValue = "") String userId,
@@ -95,13 +104,26 @@ public class IndexController {
             tags.add(earlyMap);
         }
 
-        int joinStatus = 0;
+        int joinStatus = USER_JOIN_STATUS.STATUS_NOT_JOINED.getStatusValue();
+        String oDate;
         if (userId != null && userId != "" && token != null && token != "") {
+            LOGGER.debug("userId token got!!!");
             List<Games> games = gameService.getTomorrowUserGameInfoByUid(userId);
-            joinStatus = games == null ? 0 : games.size() > 0 ? 1 : 0;
+            if (games != null && games.size() > 0) {
+                Games games1 = games.get(0);
+                String partyTime = games1.getPartyTime();
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(partyTime.substring(0, 4)).append("/").append(partyTime.substring(4, 6)).append("/").append(partyTime.substring(6));
+                oDate = stringBuilder.toString();
+                LOGGER.info("date========>" + oDate);
+                mainMap.put("date", oDate);
+                if (games1.getReChecktime() == null) {
+                    joinStatus = USER_JOIN_STATUS.STATUS_JOINED_NOTCHECK.getStatusValue();
+                } else {
+                    joinStatus = USER_JOIN_STATUS.STATUS_JOINED_CHECKED.getStatusValue();
+                }
+            }
         }
-
-
         mainMap.put("tags", tags);
         mainMap.put("total_count", totayGameCount);
         mainMap.put("content_json", contentMap);
@@ -111,8 +133,21 @@ public class IndexController {
         mainMap.put("success_count", rankNumbers.get("suc_cnt"));
         mainMap.put("fail_count", rankNumbers.get("fal_cnt"));
         mainMap.put("user_join_status", joinStatus);
+        //partyTime
+//        String current_time = calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+        mainMap.put("start_time", begin_time);
+        mainMap.put("end_time", end_time);
+        mainMap.put("current_time", getTimeFormat(new Date(), "yyyy/MM/dd HH:mm:ss"));
+
         result.put("data", mainMap);
         LOGGER.debug("{},获取项目详情", format);
         return result;
+    }
+
+
+    private String getTimeFormat(Date date, String partten) {
+        DateFormat dateFormat = new SimpleDateFormat(partten);
+        return dateFormat.format(date);
+
     }
 }
